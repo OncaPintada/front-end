ec40ade1844af49d18f288d2b7c1b65de9ff385b
Zanelato <pedeozanelato19@gmail.com>
2023-07-18 09:56:40 -0400

    Mudança width, PieGraph adicionado, Nav modificado, font modificado

--- src/components/Nav-Bar.vue
+++ src/components/Nav-Bar.vue
@@ -1,34 +1,35 @@
 <template>
-  <nav class="navbar bg-body-tertiary" style="background-color: #F0F0F0">
-    <div class="container">
-      <a class="navbar-brand" href="#">
-        <img src="../assets/logo.png" alt="Fraude Pintada" width="200" height="50">
-      </a>
+  <nav class="navbarOnca">
+    <div class="subNavOnca">
+      <img src="../assets/icon.png" alt="Fraude Pintada" class="logo">
+      <div class="nameGroup">Grupo Cionça Pindados</div>
     </div>
   </nav>
-  <nav class="navbar navbar-expand-lg custom-bg shadow">
-    <div class="container-fluid">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <router-link to="/" class="nav-link text-decoration-none">
-            <a>Introdução</a>
-          </router-link>
-          <router-link to="/analise" class="nav-link text-decoration-none">
-            <a>Análise Modelos</a>
-          </router-link>
-          <router-link to="/prevencao" class="nav-link text-decoration-none">
-            <a>Prevenção</a>
-          </router-link>
-          <router-link to="/ideia" class="nav-link text-decoration-none">
-            <a>Ideia de negócio</a>
-          </router-link>
+  <div class="nav">
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid subNav">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <router-link to="/" class="nav-link text-decoration-none">
+              <a>Introdução</a>
+            </router-link>
+            <router-link to="/analise" class="nav-link text-decoration-none">
+              <a>Análise Modelos</a>
+            </router-link>
+            <router-link to="/prevencao" class="nav-link text-decoration-none">
+              <a>Prevenção</a>
+            </router-link>
+            <router-link to="/ideia" class="nav-link text-decoration-none">
+              <a>Ideia de negócio</a>
+            </router-link>
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+  </div>
 </template>
 
 <script>
@@ -38,11 +39,64 @@ export default {}
 </script>
 
 <style>
-.custom-bg {
-  background-color: #00AE30;
+@import url('https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+.view {
+  font-family: 'Raleway', sans-serif;
 }
 
 .custom-bg .nav-link {
   color: white;
+}
+
+.logo {
+  width: 7.36%;
+  margin-left: 1.52%;
+}
+
+.subNavOnca {
+  width: 1440px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+
+.navbarOnca {
+  width: 100%;
+  height: 120px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f0f0f0;
+  box-shadow: inset 0px 2px 4px rgba(0, 0, 0, 0.25);
+  z-index: 1;
+}
+
+.nameGroup{
+  color: #000;
+  font-family: Raleway, sans-serif;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: bold;
+  margin-left: 2.63%;
+}
+.nav {
+  width: 100%;
+  background-color: #00AE30;
+}
+.navbar {
+  width: 1440px;
+  margin: 0 auto;
+}
+
+.navbar a {
+  color: #FFF;
+  text-align: center;
+  font-family: Inter, sans-serif;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,66 +1,121 @@
 <template>
   <div class="view">
-    <h1 class="text-center mt-5 mb-3">Base de Dados</h1>
-    <p class="text-center"> A <a href="https://www.kaggle.com/datasets/mlg-ulb/creditcardfraud" class="text-reset">base de
-        dados</a>
-      que iremos analisar contém transações de cartões de crédito feitas por usuários europeus em setembro de 2013. O
-      dataset é o resultado de uma transformação dos dados originais pelo algoritmo PCA <a
-        href="https://medium.com/analytics-vidhya/pca-a-linear-transformation-f8aacd4eb007" class="text-reset">(Principal
-        component analysis)</a>. Ele possui 31 atributos sendo 3 identificáveis (time, amount e class) e 28 não
-      identificáveis. A proporção dos casos de fraude pode ser vista graficamente:</p>
-    <div class="text-center">
+    <h1 class="mt-5 mb-3 title">Base de Dados</h1>
+    <p class="">
+      A
+      <a
+        href="https://www.kaggle.com/datasets/mlg-ulb/creditcardfraud"
+        class="text-reset"
+        >base de dados</a
+      >
+      que iremos analisar contém transações de cartões de crédito feitas por
+      usuários europeus em setembro de 2013. O dataset é o resultado de uma
+      transformação dos dados originais pelo algoritmo PCA
+      <a
+        href="https://medium.com/analytics-vidhya/pca-a-linear-transformation-f8aacd4eb007"
+        class="text-reset"
+        >(Principal component analysis)</a
+      >. Ele possui 31 atributos sendo 3 identificáveis (time, amount e class) e
+      28 não identificáveis. A proporção dos casos de fraude pode ser vista
+      graficamente:
+    </p>
+    <div class="paiPie mb-3">
       <!-- Gráfico - Proporção de casos de fraude -->
-
-      <h3>grafico aqui</h3>
+      <PieGraph class="pie" />
     </div>
 
     <table class="table table-striped">
       <!-- Tabela de dados -->
-
     </table>
-    <p class="text-center">Como podemos observar, o dataset é muito desbalanceado, possuindo pouquíssimos casos de fraude.
-      Já seus atributos variam de forma diferente, como é possível observar:</p>
-    <div class="text-center">
+    <p class="">
+      Como podemos observar, o dataset é muito desbalanceado, possuindo
+      pouquíssimos casos de fraude. Já seus atributos variam de forma diferente,
+      como é possível observar:
+    </p>
+    <div class="">
       <!-- Gráfico - Variação dos atributos -->
-      
+
       <h3>grafico aqui</h3>
     </div>
-    <div class="text-center dropdown">
-      <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    <div class="dropdown">
+      <button
+        class="btn btn-primary dropdown-toggle"
+        type="button"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
         Escolher Atributo
       </button>
 
       <ul class="dropdown-menu">
         <!-- Opções de atributos -->
-        <li><a class="dropdown-item" href="#">atributo1</a></li>
-        <li><a class="dropdown-item" href="#">atributo2</a></li>
-        <li><a class="dropdown-item" href="#">atributo3</a></li>
+        <li class="dropdown-item">atributo1</li>
+        <li class="dropdown-item">atributo2</li>
+        <li class="dropdown-item">atributo3</li>
       </ul>
     </div>
-    <p class="text-center">Percebemos que a maior parte deles se concentra em um intervalo pequeno de valores, ou seja,
-      possuem um grau alto de compactação. É possível analisar a correlação dos atributos com sua classificação,
-      estabelecendo que 1 (ou -1) é a correlação máxima (proporcionais) e 0 é a mínima (sem correlação):</p>
-    <div class="text-center">
+    <p class="">
+      Percebemos que a maior parte deles se concentra em um intervalo pequeno de
+      valores, ou seja, possuem um grau alto de compactação. É possível analisar
+      a correlação dos atributos com sua classificação, estabelecendo que 1 (ou
+      -1) é a correlação máxima (proporcionais) e 0 é a mínima (sem correlação):
+    </p>
+    <div class="">
       <!-- Gráfico - Correlação dos atributos -->
       <h3>grafico aqui</h3>
     </div>
-    <p class="text-center">Percebemos que há uma correlação negativa da classe com as colunas v1, v3, v5, v6, v7, v10,
-      v12, v14, v16, v17 e v18 e uma correlação positiva com os atributos v2, v4 e v11. Agora vamos visualizar como são os
-      valores dos atributos de transações fraudulentas e não fraudulentas:</p>
-    <div class="text-center">
+    <p class="">
+      Percebemos que há uma correlação negativa da classe com as colunas v1, v3,
+      v5, v6, v7, v10, v12, v14, v16, v17 e v18 e uma correlação positiva com os
+      atributos v2, v4 e v11. Agora vamos visualizar como são os valores dos
+      atributos de transações fraudulentas e não fraudulentas:
+    </p>
+    <div class="">
       <!-- Gráfico - Valores dos atributos -->
       <h3>grafico aqui</h3>
     </div>
-    <p class="text-center">Através do gráfico, vemos que os atributos que mais se diferem entre as classes (fraude ou não
-      fraude) são esses: v3, v7, v9, v10, v11, v12, v14, v16, v17, v18.</p>
+    <p class="">
+      Através do gráfico, vemos que os atributos que mais se diferem entre as
+      classes (fraude ou não fraude) são esses: v3, v7, v9, v10, v11, v12, v14,
+      v16, v17, v18.
+    </p>
   </div>
 </template>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap");
+
 .view {
   margin: 0 auto;
-  width: 90%;
+  width: 1100px;
+  color: #2d2d2d;
+  font-family: Inter;
+  font-size: 23px;
+  font-style: normal;
+  font-weight: 500;
+}
+
+.view a:hover {
+  color: #00ae30;
+}
+
+.title {
+  color: #2d2d2d;
+  font-family: Inter;
+  font-size: 45px;
+  font-style: normal;
+  font-weight: 700;
+}
+.paiPie {
+  width: 60%;
+  margin: auto;
 }
 </style>
-
-
+<script>
+import PieGraph from "@/components/PieGraph.vue";
+export default {
+  components: { PieGraph },
+};
+</script>
